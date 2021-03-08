af69796816043d901275b2e28b05e2f35f7dc604
Icarus <ibuckhold@gmail.com>
2021-03-08 16:17:04 -0700

    Finished out sign up authentication and Demo user is set up

--- routes/users.js
+++ routes/users.js
@@ -58,10 +58,94 @@ router.post("/login", csrfProtection, loginValidators, asyncHandler(async (req, 
 }));
 
 
-router.get("/signup", (req, res) => {
-  // GET mediumverse.com/users/signup
-
+router.get("/signup", csrfProtection, (req, res) => {
+  const user = User.build();
+  res.render("user-signup", {
+    title: "Sign Up",
+    user,
+    csrfToken: req.csrfToken()
+  });
   // check for email length
 });
+
+const signUpValidator = [
+  check('username')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Username')
+    .isLength({ max: 50 })
+    .withMessage('Username must not be more than 50 characters long')
+    // .isUnique()  is this functionable
+    // .withMessage('That username is already in use.'),
+    .custom((value) => {
+      return User.findOne({ where: { username: value } })
+        .then((user) => {
+          if (user) {
+            return Promise.reject('The provided Username is already in use by another account');
+          }
+        });
+    }),
+  check('email')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Email Address')
+    .isLength({ max: 50 })
+    .withMessage('Email Address must not be more than 50 characters long')
+    .isEmail()
+    .withMessage('Email Address is not a valid email')
+    .custom((value) => {
+      return User.findOne({ where: { email: value } })
+        .then((user) => {
+          if (user) {
+            return Promise.reject('The provided Email Address is already in use by another account');
+          }
+        });
+    }),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Password')
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, 'g')
+    .withMessage('Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'),
+  check('confirmPassword')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Confirm Password')
+    .isLength({ max: 50 })
+    .withMessage('Confirm Password must not be more than 50 characters long')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Confirm Password does not match Password');
+      }
+      return true;
+    }),
+]
+
+router.post('/signup', csrfProtection, signUpValidator, asyncHandler(async(req, res) => {
+  const {
+    email,
+    username,
+    password
+  } = req.body;
+
+  const user = User.build({
+    email,
+    username
+  });
+
+  const validatorErrors = validationResult(req);
+  if (validatorErrors.isEmpty()) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    user.hashedPassword = hashedPassword;
+    await user.save();
+    loginUser(req, res, user);
+    res.redirect('/');
+  } else {
+    const errors = validatorErrors.array().map((error) => error.msg);
+    res.render('user-signup', {
+      title: "Sign Up",
+      user,
+      errors,
+      csrfToken: req.csrfToken()
+    });
+  }
+}));
+
 
 module.exports = router;
