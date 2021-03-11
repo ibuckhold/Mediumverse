64cc38466a8ed895672697c9c384264524232f3c
Icarus <ibuckhold@gmail.com>
2021-03-11 10:09:32 -0700

    Almost have Heroku set up

--- config/database.js
+++ config/database.js
@@ -12,8 +12,12 @@ module.exports = {
     seederStorage: 'sequelize',
   },
   production: {
-    use_env_variable: 'postgres://zvpyymiobgavlv:a557370a1f2b00e425951cfb7f9808381511872aa0dcc5a44a9587a5957dedc7@ec2-54-164-241-193.compute-1.amazonaws.com:5432/d3d8t2dvi6km5s',
+    use_env_variable: "DATABASE_URL",
     dialect: 'postgres',
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    },
     seederStorage: 'sequelize'
   }
 };
