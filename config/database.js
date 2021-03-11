e5c7f8f264b213e6239862033b1fb97cb93fdec8
Icarus <ibuckhold@gmail.com>
2021-03-11 09:47:38 -0700

    Set up our heroku with our project

--- config/database.js
+++ config/database.js
@@ -11,4 +11,9 @@ module.exports = {
     dialect: 'postgres',
     seederStorage: 'sequelize',
   },
+  production: {
+    use_env_variable: 'postgres://zvpyymiobgavlv:a557370a1f2b00e425951cfb7f9808381511872aa0dcc5a44a9587a5957dedc7@ec2-54-164-241-193.compute-1.amazonaws.com:5432/d3d8t2dvi6km5s',
+    dialect: 'postgres',
+    seederStorage: 'sequelize'
+  }
 };
